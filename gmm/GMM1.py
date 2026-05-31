c103031884e8023caf8befabfaf57fb50df8c9ce
YSK <yskim@pebble-square.com>
2026-05-31 17:07:42 +0900

    move GMM file and rename

---
+++ gmm/GMM1.py
@@ -0,0 +1,39 @@
+from sklearn.mixture import GaussianMixture
+from sklearn.datasets import load_iris
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+
+iris = load_iris()
+features_names = ['sl','sw', 'pl', 'pw']
+
+irisDF = pd.DataFrame(data=iris.data,\
+                        columns = features_names)
+irisDF['target'] = iris.target
+gmm = GaussianMixture(n_components=3,\
+    random_state=17).fit(iris.data)
+
+gmm_labels = gmm.predict(iris.data)
+
+irisDF['gmm_cluster'] = gmm_labels
+print(\
+    irisDF.groupby('target')['gmm_cluster'].value_counts())
+xynew = gmm.sample(10)
+
+n_c = np.arange(1,21)
+models=[GaussianMixture(n,\
+    random_state=17).fit(iris.data) for n in n_c]
+
+plt.plot(\
+    n_c , [m.bic(iris.data) for m in models], label = 'BIC')
+
+plt.plot(\
+    n_c, [m.aic(iris.data) for m in models], label = 'AIC')
+
+plt.legend()
+plt.xlabel('n_components')
+plt.savefig('iris_gmm.png')
+
+
